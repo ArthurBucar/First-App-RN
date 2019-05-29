4b84cff6c1e30a6116baed02bed42c8b6993399b
Arthur Bucar <arthur.bucar@hotmail.com>
2019-05-28 21:03:42 -0300

    commit sobre o app receber uma API e retornar no console.

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import {View, Text } from 'react-native';
-
+import api from '../services/api';
 
 export default class Main extends Component {
 
     static navigationOptions = {
         title: "Projeto"
+    };
+
+    componentDidMount(){
+        this.loadProducts();
+    
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products'); 
+
+        const { docs } = response.data;
+
+        console.log(docs);
     };
 
     render(){
